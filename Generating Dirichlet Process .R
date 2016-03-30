d69f63aef76c53e5e98126f5b7a654d2ee0d566d
Smutek-k <Smutek-k@users.noreply.github.com>
2016-03-30 13:53:42 +0200

    First part of a project

---
+++ Generating Dirichlet Process .R
@@ -0,0 +1,18 @@
+
+n <- 3
+alpha <- 3
+v <- rbeta(n,alpha,1)
+temp <- (1-v)
+
+temp <- cumprod(temp)
+temp <- c(1,temp[-n])
+sample <- rnorm(n)
+weights <- v*temp
+sample <-()
+dirP <- function()
+{
+  
+}
+
+#generating 
+stickbrek <- 
