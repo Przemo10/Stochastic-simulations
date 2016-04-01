53d37d85dd933d0c85562af311c99daaa1f7f946
Smutek Krzysztof <krzysztof.m.smutek@gmail.com>
2016-04-01 12:16:35 +0200

    Przed dodaniem testu

--- Generating Dirichlet Process .R
+++ Generating Dirichlet Process .R
@@ -1,18 +1,46 @@
+#http://www.mimuw.edu.pl/~bmia/symstoch/Projekty/Procesy%20Dirichleta/opis.pdf
+#czesc: Generator procesu Dirichlet
 
-n <- 3
-alpha <- 3
-v <- rbeta(n,alpha,1)
-temp <- (1-v)
-
-temp <- cumprod(temp)
-temp <- c(1,temp[-n])
-sample <- rnorm(n)
-weights <- v*temp
-sample <-()
-dirP <- function()
+#Task 1
+# probab wylicza wagi dla skonczonej liczby punktow w procesie Dirichleta
+# trzeba ja zmodyfikowac tak zeby zamiast brac n, brala epsilon
+# gdzie epsilon bliski zera (np 0.05) 
+# a jako wynik zwracala tyle losowych wag ile akurat bedzie potrzeba
+# zeby (1-sum(weights))=epsilon (pewnie jakims while-m, ale wazne zeby
+# nie losowac wag od nowa tylko dokladac)
+probab <- function(n,alpha)
 {
-  
+  v <- rbeta(n,1,alpha)
+  temp <- (1-v)
+  temp <- cumprod(temp)
+  temp <- c(1,temp[-n])
+  weights <- v*temp
+  sum(weights)
+  return (weights)
 }
 
-#generating 
-stickbrek <- 
+#generowanie probki z pierwotnego rozkladu 
+parent_sample <-rnorm(n)
+
+#generating one observation from Dirichlet process (with parent distribution Gaussian)
+obs <- function(weights)
+{
+nowe <- Inf
+probka <- sample(c(parent_sample,nowe),1,replace = TRUE, prob = c(weights,(1-sum(weights))))
+if (probka == c(Inf)) (probka <- rnorm(1))
+return (probka)
+}
+#generating sample 
+sample <- replicate(10,obs(weights)) 
+
+#Task 2 
+#jezeli bedziemy losowali z rozkladu dyskretnego (np. jednosnajnego na {1,2,..,10})
+#to niektore elementy parent_sample beda sie powtarzaly
+# trzeba przerobic ta funkcje tak, zeby skleic wagi odpowiadajace powtorzeniom 
+
+#testowanie czy otrzymujemy wlasciwy rozklad (to jest to nad czym ja teraz pracuje)
+sum(weights[-1])
+plot(dbeta(kwantyle,1,3))
+kwantyle <- seq(length = 100, from = 0, to = 1)
+
+
